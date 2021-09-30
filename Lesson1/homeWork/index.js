63e2e9b7edb551900f8a5f06c8cc03a6eae10edf
Alexandr Belov <sanya7belyi@gmail.com>
2021-09-30 23:21:07 +0300

    Format repository

---
+++ Lesson1/homeWork/index.js
@@ -0,0 +1,31 @@
+console.log("Hello World");
+const userName = "Alex";
+console.log(userName);
+console.log(typeof userName);
+let userAge = 26;
+console.log(userAge);
+console.log(typeof userAge);
+let hasChildren = false;
+console.log(hasChildren);
+console.log(typeof hasChildren);
+let userProffesion = null;
+console.log(userProffesion);
+console.log(typeof userProffesion);
+let userLogin
+console.log(userLogin);
+console.log(typeof userLogin);
+let userFull = {
+    name: "Alex", 
+    sex: "male"
+};
+console.log(userFull);
+console.log(typeof userFull);
+const symbol = Symbol("Dragon")
+console.log(symbol);
+console.log(typeof symbol);
+let viewShapeCount = 100200030010012087564544n;
+console.log(viewShapeCount);
+console.log(typeof viewShapeCount);
+
+userFull = 12;
+console.log(userFull);
