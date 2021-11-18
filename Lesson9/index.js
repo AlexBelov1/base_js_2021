dc59a860b5e9947bc6c6abf6404d45f8661fd615
Alexandr Belov <sanya7belyi@gmail.com>
2021-11-19 00:42:49 +0300

    afterlesson

--- Lesson9/index.js
+++ Lesson9/index.js
@@ -45,3 +45,78 @@
 // };
 // oneCard.setFrom("Max");
 // console.log(oneCard);
+
+// class Parent {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   sayName() {
+//     return `Привет, мое имя ${this.name}`;
+//   }
+// }
+
+// const firstParent = new Parent("Max");
+// // console.log(firstParent);
+// // console.log(firstParent.sayName());
+// class Child extends Parent {
+//   constructor(name, lastName) {
+//     super(name);
+//     this.lastName = lastName;
+//   }
+//   sayName() {
+//     console.log("Это внутренний метод");
+//   }
+//   sayFullName() {
+//     const nameString = `${super.sayName()} ${this.lastName}`;
+//     return nameString;
+//   }
+//   get lastName() {
+//     return this._lastName;
+//   }
+//   set lastName(newValue) {
+//     this._lastName = newValue;
+//   }
+//   getLastName() {
+//     return this.lastName;
+//   }
+//   setLastName(newValue) {
+//     this.lastName = newValue;
+//   }
+// }
+
+// const firstChild = new Child("Ivan", "Petrov");
+// console.log(firstChild);
+// firstChild.setLastName("Smirnov");
+// firstChild.lastName("Smirnov");
+// console.log(firstChild.getLastName());
+// console.log(firstChild.lastName);
+
+class People {
+  constructor(name, surname) {
+    this.name = name;
+    this.surname = surname;
+  }
+  getFullName() {
+    return `Полное имя ${this.name} ${this.surname}`;
+  }
+}
+const firstPeople = new People("Ivan", "Ivanovich");
+console.log(firstPeople.getFullName());
+
+class Worker extends People {
+  constructor(name, surname, rate, day) {
+    super(name, surname);
+    this.rate = rate;
+    this.day = day;
+  }
+  getSalary() {
+    return `${this.getFullName()}, Зарплата за ${this.day} дней составляет ${
+      this.day * this.rate
+    }`;
+  }
+}
+const one = new Worker("Alex", "Belov", 1000, 100);
+console.log(one.getSalary());
+
+const secondWorker = new Worker("Petr", "Petrovich", 2900, 20);
+console.log(secondWorker.getSalary());
